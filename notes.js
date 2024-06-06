f9fa1205edd9753c5cb3293ab36f5d748de1296d
DIVYAM SHARMA NIT <ds7081241904@gmail.com>
2024-06-06 22:19:54 +0530

    eleventh commit

---
+++ notes.js
@@ -0,0 +1,13 @@
+let name="divyam sharma"
+let about="you are the best part of my life."
+
+let age ="19"
+let college="NIT DURGAPUR"
+
+
+// revert do the changes that can be committed.
+// but reset  do not do the changes and hence they can't be committed.
+// here git remote is to set the server to which we want to connect our git
+     // syntax is git remote -v  (to check whether the remote is set or not)
+     // syntax is git remote add <name of the server> <https link>
+// here by default the <name of the server> is origin.
